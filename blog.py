7a163e63cff7f356f1d99ee5b7a2f01fadd85eb8
Young19-haha <54867684+Young19-haha@users.noreply.github.com>
2020-11-08 09:32:32 -0500

    Update blog.py

--- blog.py
+++ blog.py
@@ -2,7 +2,8 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 import requests
-from BeautifulSoup4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+import BeautifulSoup
 
 def popualtion_GRP(dataset):
     selection = alt.selection_single(on='mouseover',empty='none')
