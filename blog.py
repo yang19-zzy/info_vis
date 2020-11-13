a6a21fb0c5e18bde5aa563f326140bd1c52bcfa3
Young19-haha <54867684+Young19-haha@users.noreply.github.com>
2020-11-12 20:50:20 -0500

    Add files via upload

--- blog.py
+++ blog.py
@@ -1,10 +1,18 @@
 import altair as alt
 import pandas as pd
+import numpy as np
+import seaborn as sns
 import streamlit as st
+from matplotlib import pyplot as plt
+# from preprocessing import *
 # import requests
 # from bs4 import BeautifulSoup
 
-def popualtion_GRP(dataset):
+##################
+## build graphs ##
+##################
+
+def popualtion_GDP_stat(dataset):
     selection = alt.selection_single(on='mouseover',empty='none')
 
     point_1 = alt.Chart(dataset).mark_point(filled=True,size=90).encode(
@@ -28,26 +36,78 @@ def popualtion_GRP(dataset):
 
     return point_1 + point_2 + text
 
-def NOR_medals(dataset):
-    return alt.Chart(dataset).mark_area().encode(
-    x=alt.X('Year:O'),
-    y=alt.Y('count():Q',axis=alt.Axis(grid=False)),
-    color=alt.Color('Medal:N', sort=['Gold','Silver','Bronze'])
+def popualtion_GDP_inter(dataset,filter):
+    selection = alt.selection_single(on='mouseover',empty='none')
+
+    point_1 = alt.Chart(dataset,title='Population - GDP per Capita').mark_point(filled=True,size=90).encode(
+        x=alt.X('Population:Q'),
+        y=alt.Y('GDP per Capita:Q'),
+        size='Population',
+        tooltip=['Country','Code']
+    ).add_selection(selection).transform_filter(
+        alt.FieldOneOfPredicate(field='Country', oneOf=filter)
     )
 
-def medal_rank(dataset):
-    return alt.Chart(dataset).mark_bar().encode(
-    y=alt.Y('Country:N',sort='-x'),
-    x=alt.X('Total:Q')
-    ).transform_joinaggregate(
-        total = 'sum(Total)',
-        groupby=['Country']
-    ).transform_window(
-        rank='rank(total)',
-        sort=[alt.SortField('total',order='descending')]
+    point_2 = alt.Chart(dataset).mark_point(filled=True,size=90).encode(
+        x=alt.X('Population:Q'),
+        y=alt.Y('GDP per Capita:Q'),
+        size='Population',
+        color=alt.value('red')
     ).transform_filter(
-        alt.datum.rank<10
+        alt.datum.Country == 'Norway'
     )
+
+    text = alt.Chart(dataset).encode(
+        x=alt.X('Population:Q'),
+        y=alt.Y('GDP per Capita:Q'),
+        text=alt.Text('Country'),
+        color=alt.value('black')
+    ).mark_text(dy=-4,dx=25).transform_filter(
+        alt.FieldOneOfPredicate(field='Country', oneOf=filter)
+    )
+
+    return (point_1 + point_2 + text).interactive()
+
+def NOR_medals_stat(dataset):
+    return alt.Chart(dataset,title="Chnages of Number of Medals in Norway 1900-2014").mark_area().encode(
+    x=alt.X('Year:O'),
+    y=alt.Y('count():Q',axis=alt.Axis(grid=False)),
+    color=alt.Color('Medal:N'))
+
+def NOR_medals_inter(dataset, filter_year):
+    return alt.Chart(dataset,title="Chnages of Number of Medals in Norway").mark_area().encode(
+    x=alt.X('Year:O'),
+    y=alt.Y('count():Q',axis=alt.Axis(grid=False),scale=alt.Scale(domain=(0,160))),
+    color=alt.Color('Medal:N', 
+                    sort=['Gold','Silver','Bronze'],
+                    scale=alt.Scale(domain=['Gold','Silver','Bronze'],
+                                    range=['gold', 'silver','darkorange']))
+    ).transform_filter(
+        alt.datum.Year >= filter_year
+    )
+
+def medal_rank(dataset, medal_type='Total'):
+    base = alt.Chart(dataset,title='2018 Medal Summary').mark_bar().encode(
+            y=alt.Y('Country:N',sort='-x'),
+            x=alt.X('total:Q',scale=alt.Scale(domain=(0,40)))
+            ).transform_joinaggregate(
+                total = f'sum({medal_type})',
+                groupby=['Country']
+            ).transform_window(
+                rank='rank(total)',
+                sort=[alt.SortField('total',order='descending')]
+            ).transform_filter(
+                alt.datum.rank<10
+            )
+    if medal_type == 'Gold':
+        return base.mark_bar(color='gold')
+    elif medal_type == 'Silver':
+        return base.mark_bar(color='silver')
+    elif medal_type == 'Bronze':
+        return base.mark_bar(color='darkorange')
+    else:
+        return base
+    
 
 def gender(dataset):
     return alt.Chart(dataset).mark_bar().encode(
@@ -56,102 +116,162 @@ def gender(dataset):
     color='Gender'
     )
 
+def gender_inter(dataset, filter_year):
+    char_m = alt.Chart(dataset).mark_bar(color="lightblue").encode(
+    x=alt.X('count:Q',
+            title='Men',
+            sort='descending',
+           scale=alt.Scale(domain=(0,160)),
+           axis=alt.Axis(grid=False)),
+    y=alt.Y('Year:N'),
+    tooltip=['count:Q']
+    ).transform_joinaggregate(
+        groupby=['Year','Gender'],
+        count = 'count(Medal)'
+    ).transform_filter(
+        (alt.datum.Gender=='Men') & (alt.datum.Year >= filter_year)
+    )
+
+    chart_f = alt.Chart(dataset).mark_bar(color="lightpink").encode(
+        x=alt.X('count:Q',
+                title='Women',
+                sort='ascending',
+            scale=alt.Scale(domain=(0,160)),
+           axis=alt.Axis(grid=False)),
+        y=alt.Y('Year:N',title=None,axis=None),
+        tooltip=['count:Q']
+    ).transform_joinaggregate(
+        groupby=['Year','Gender'],
+        count = 'count(Medal)'
+    ).transform_filter(
+        (alt.datum.Gender=='Women') & (alt.datum.Year >= filter_year)
+    )
+
+    return (char_m | chart_f).properties(title='Number of Winners in Gender')
+
+def season_inter(dataset, filter_year):
+    if filter_year < 2014:
+        g = sns.barplot(x='Year',y='Medal',data=dataset[dataset.Year.isin(range(filter_year,2014))],hue='Season')
+    else:
+        g = sns.barplot(x='Year',y='Medal',data=dataset[dataset.Year.isin(['2014'])],hue='Season')
+    g.set_xticklabels(labels=g.get_xticklabels(), rotation=45)
+    return g
 
 def main():
-    # Data Processing
+    # load data
     ## dataset_1
-    ## https://www.kaggle.com/the-guardian/olympic-games?select=summer.csv
-    dictionary = pd.read_csv("archive/dictionary.csv")
-    summer = pd.read_csv("archive/summer.csv")
-    winter = pd.read_csv("archive/winter.csv")
-    countries = dictionary.dropna()
-    country_code = countries.Code.to_list()
-    ## winter_countries
-    winter_countries = winter.loc[winter['Country'].isin(country_code),:]
-    winter_countries['Season'] = ['winter'] * winter_countries.shape[0]
-    ## summer_countries
-    summer_countries = summer.loc[summer['Country'].isin(country_code)]
-    summer_countries['Season'] = ['summer'] * summer_countries.shape[0]
-    ## df
-    df = pd.concat([summer_countries,winter_countries],axis=0).reset_index().iloc[:,1:]
+    countries = pd.read_csv('data/countries.csv')
+    df = pd.read_csv('data/summer_winter.csv')
+    ## dataframe used in stat
     df_50_60 = df[df.Year.isin(range(1950,1970))]
     df_70_80 = df[df.Year.isin(range(1970,1990))]
     df_90 = df[df.Year.isin(range(1990,1999))]
     df_00 = df[df.Year.isin(range(2000,2006))]
     df_06 = df[df.Year>2006]
     df_NOR = df[df.Country=='NOR']
+    # country_code = list(countries.Code)
     ## dataset_2
-    medal_2018 = pd.read_csv('archive/medal_2018.csv')
-    # URL = "https://www.nytimes.com/interactive/2018/sports/olympics/medal-count-results-schedule.html"
+    medal_2018 = pd.read_csv('data/medal_2018.csv')
+    ## dataset_3
+    winner_medals = pd.read_excel('archive/Winer Olympic Medals.xlsx')
 
-    # result = requests.get(URL,'html.parser')
-    # soup = BeautifulSoup(result.content, 'html.parser')
-    # table_soup = soup.find(class_="int-table oly-table int-table-medal-standings")
+    ##########################
+    ####### main page ########
+    ##########################
 
-    # table = table_soup.find('table')
-    # rows_soup = table.find_all('tr')
-    # heads = rows_soup[0]
-    # data = rows_soup[1:]
+    st.title("""Olympics' Facts in Norway""")
 
-    # columns = []
-    # heads_item = heads.find_all('td')
-    # for item in heads_item:
-    #     columns.append(item.text)
-    # rows = []
-    # for row in data:
-    #     rows.append([item.text for item in row])
-
-    # medal_2018 = pd.DataFrame(rows,columns=columns)
-    # medal_2018['Country'] = medal_2018.apply(lambda X: X['Medal Count'][:-3],axis=1)
-    # medal_2018['Code'] = medal_2018.apply(lambda row: row['Medal Count'][-3:], axis=1)
+    st.sidebar.markdown("""Select to show more contents""")
+    st.sidebar.text("(Diselect to show less)")
+    checkbox_1 = st.sidebar.checkbox('Domain Questions',value=True)
+    checkbox_2 = st.sidebar.checkbox('Select to learn more',value=False)
+    checkbox_3 = st.sidebar.checkbox('Design Process',value=False)
 
 
+    html = """
+    <style>
+    .sidebar .sidebar-content {
+        background-image: linear-gradient(#ffe6e6);
+        padding: 80px 50px;
 
+    }
+    .sidebar .sidebar-collapse-control,
+    .sidebar.--collapsed .sidebar-collapse-control {
+        left: 2px;
+        right: auto;
+    }
 
-
-
-    st.title("""Olympics Facts in Norway\nCommunicative Visualization """)
-    # st.altair_chart()
-    selectbox_values = st.sidebar.selectbox(label='Select to show more',
-                    options=['Learning Objectives',
-                            'Design Process',
-                            'Final Design'])
-    if selectbox_values == 'Learning Objectives':
-        st.header('Learning Objectves')
+    </style>
+    """
+    st.markdown(html, unsafe_allow_html=True)
+    
+    if checkbox_1:
+        st.header('Domain Questions')
         st.markdown("""
-        1. What is the difference in number of medals between winter and summer competition?
-        2. From 1950 to 2014, how number of athletes in different gender has changed?
-        3. What is the change in total number of medals in Norway?
+        1. In 2018, what are the top 10 countries with the highest number of medals? 
+        2. How population and GDP per capita relate between countries?
+        3. What is the difference in the number of medals between winter and summer competition?
+        4. How the number of winners with different genders has changed?
+        5. What is the change in the total number of medals in Norway?
+
         """)
 
-    elif selectbox_values == 'Design Process':
-        st.header('Design Process - My exploration with data')
+    
+    if checkbox_2:
+        st.markdown('----------------------')
+        # st.balloons()
+        st.header("Learn Something About Olympics' Facts in Norway")
+        # - Default view at the beginning of the page
+        st.markdown('<h3>Top 10 Countries with Different Medals in 2018</h3>',unsafe_allow_html=True)
+        medal_type = st.radio(label='Select medal type for 2018',options=['Total','Gold','Silver','Bronze'])
+        st.altair_chart(medal_rank(medal_2018,medal_type), use_container_width=True)
+        
+        st.sidebar.markdown('----------------------')
+        year = st.sidebar.slider('Year', 1896, 2014, 1988)  
+        country_list = list(countries.Country) 
+        button = st.sidebar.multiselect('Select to show more countries', country_list, default=['Norway'])
+        
+        st.markdown('----------------------')
+        st.markdown('<h3>Relations between Population and GDP per Capita Overtime</h3>',unsafe_allow_html=True)
+        st.altair_chart(popualtion_GDP_inter(countries,button), use_container_width=True)
+        # st.text(str(year))
+
+        st.markdown('----------------------')
+        st.markdown('<h3>Number of Medals in Summer and Winter Olympics Overtime 1896-2012</h3>',unsafe_allow_html=True)
+        year_season_medal = df.groupby(['Year','Season']).count().Medal.reset_index()
+        plt.subplots(figsize=(7,3))
+        g = season_inter(year_season_medal, year)
+        g.set(title='Number of Medals in Summer and Winter Olympics overtime')
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.pyplot()
+
+        st.markdown('----------------------')
+        st.markdown('<h3>Number of Winners in Different Gender Changes Overtime 1924-2014</h3>',unsafe_allow_html=True)
+        st.altair_chart(gender_inter(winner_medals,year), use_container_width=True)
+        # gender_fig.configure_view(title='test')
+
+        st.markdown('----------------------')
+        st.markdown('<h3>Number of medals change along years in Norway</h3>',unsafe_allow_html=True)
+        st.altair_chart(NOR_medals_inter(df_NOR,year), use_container_width=True)
+
+    if checkbox_3:
+        st.markdown('----------------------')
+        st.header('Design Process')
+        st.markdown('<h3>Exploratory Data Analysis</h3>',unsafe_allow_html=True)
         st.markdown(" ")
         st.markdown('Relationship between popultaion and GDP')
-        st.altair_chart(popualtion_GRP(countries), use_container_width=True)
+        st.altair_chart(popualtion_GDP_stat(countries), use_container_width=True)
 
         st.markdown('Number of medals change along years in Norway')
-        st.altair_chart(NOR_medals(df_NOR), use_container_width=True)
+        st.altair_chart(NOR_medals_stat(df_NOR), use_container_width=True)
 
         st.markdown('Number of athletes with different genders changes overtime')
         st.altair_chart(gender(df_06)|gender(df_00)|gender(df_90)|gender(df_70_80)|gender(df_50_60), use_container_width=True)
 
         st.markdown('Top 10 total medals in 2018')
         st.altair_chart(medal_rank(medal_2018), use_container_width=True)
-    else:
-        st.header("Final Design")
-        st.markdown("Why I think this is a good design?")
-        st.markdown("""
-        1. I would combine created graphs together with filters 
-        2. xxx
-        """)
+
         
-        
-        st.markdown("How would I evaluate my design?")
-        st.markdown("""
-        1. 
-        2. 
-        """)        
 
 
 
---
+++ preprocessing.py
@@ -0,0 +1,71 @@
+import altair as alt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import streamlit as st
+from matplotlib import pyplot as plt
+import requests
+from bs4 import BeautifulSoup
+
+# Data Processing
+## dataset_1
+## https://www.kaggle.com/the-guardian/olympic-games?select=summer.csv
+dictionary = pd.read_csv("archive/dictionary.csv")
+summer = pd.read_csv("archive/summer.csv")
+winter = pd.read_csv("archive/winter.csv")
+countries = dictionary.dropna()
+country_code = countries.Code.to_list()
+## winter_countries
+winter_countries = winter.loc[winter['Country'].isin(country_code)]
+winter_countries['Season'] = ['winter'] * winter_countries.shape[0]
+## summer_countries
+summer_countries = summer.loc[summer['Country'].isin(country_code)]
+summer_countries['Season'] = ['summer'] * summer_countries.shape[0]
+## df
+df = pd.concat([summer_countries,winter_countries],axis=0).reset_index().iloc[:,1:]
+df_50_60 = df[df.Year.isin(range(1950,1970))]
+df_70_80 = df[df.Year.isin(range(1970,1990))]
+df_90 = df[df.Year.isin(range(1990,1999))]
+df_00 = df[df.Year.isin(range(2000,2006))]
+df_06 = df[df.Year>2006]
+df_NOR = df[df.Country=='NOR']
+
+df.to_csv('data/summer_winter.csv')
+countries.to_csv('data/countries.csv')
+
+
+## dataset_2
+URL = "https://www.nytimes.com/interactive/2018/sports/olympics/medal-count-results-schedule.html"
+
+result = requests.get(URL,'html.parser')
+soup = BeautifulSoup(result.content, 'html.parser')
+table_soup = soup.find(class_="int-table oly-table int-table-medal-standings")
+
+table = table_soup.find('table')
+rows_soup = table.find_all('tr')
+heads = rows_soup[0]
+data = rows_soup[1:]
+
+columns = []
+heads_item = heads.find_all('td')
+for item in heads_item:
+    columns.append(item.text)
+rows = []
+for row in data:
+    rows.append([item.text for item in row])
+
+medal_2018 = pd.DataFrame(rows,columns=columns)
+medal_2018['Country'] = medal_2018.apply(lambda X: X['Medal Count'][:-3],axis=1)
+medal_2018['Code'] = medal_2018.apply(lambda row: row['Medal Count'][-3:], axis=1)
+
+medal_2018.to_csv('data/medal_2018.csv')
+
+
+## dataset-3
+# https://data.world/makeovermonday/2018w7-the-winter-olympics 
+# Winner of Winter Olympics 1924-2014
+winner_medals = pd.read_excel('archive/Winer Olympic Medals.xlsx')
+
+
+
+
